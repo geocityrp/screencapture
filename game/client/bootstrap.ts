cbb61e83b8f5e38d48df13519224c097015aa857
itschip <chip@projecterror.dev>
2026-04-19 16:25:44 +0200

    fix(game/client): yet another callbackUrl fix

--- game/client/bootstrap.ts
+++ game/client/bootstrap.ts
@@ -127,7 +127,7 @@ function requestScreenshot(options: CaptureRequest, callback: RequestScreenshotU
 
   createImageCaptureMessage({
     ...realOptions,
-    callbackUrl: `${serverEndpoint}/screenshot_created`,
+    callbackUrl: `https://${GetCurrentResourceName()}/screenshot_created`,
     correlationId,
   });
 }
